cdd0b760cfdd4bb4ff40ec602a6a2b15bb6ae230
Vadim <qweek2@ya.ru>
2020-10-26 20:48:09 +0300

    Update RunFits.sh

--- RunFits.sh
+++ RunFits.sh
@@ -1,10 +1,7 @@
-
-for run in {0..10}; 
-do  
+for run in {0..10};
+do
     echo -------------------------
     echo --------- $run -------------
     echo -------------------------
-
-
     root -l -b -q 'FitIt.cpp('$run')'
 done
