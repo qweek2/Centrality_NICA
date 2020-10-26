8d9eb067ec5596ecd77c1b369a4b164bd59e7b22
Vadim <qweek2@ya.ru>
2020-10-26 20:45:55 +0300

    update

--- FitIt.cpp
+++ FitIt.cpp
@@ -17,35 +17,36 @@ using namespace std;
 
 void FitIt(int run)
 {
-	TFile* f_input = new TFile("EdepEmax_SMM_full_1_to_1.root");
-	TH2F* hist = (TH2F*)f_input->Get("EdepEmax");
+	TFile *f_input = new TFile("EdepEmax_SMM_full_1_to_1.root");
+	TH2F *hist = (TH2F *)f_input->Get("EdepEmax");
 	TCanvas *canvas = new TCanvas();
-	TGraph* g = new TGraph(); // using the blank constructor
-	
-	double count =0.;
+	TGraph *g = new TGraph(); // using the blank constructor
+
+	double count = 0.;
 	int Bin_Y = hist->GetYaxis()->GetNbins();
-	int Bin_X = hist->GetXaxis()->GetNbins();	
-	   TMarker *m;
+	int Bin_X = hist->GetXaxis()->GetNbins();
+	TMarker *m;
 
-	cout << hist->GetYaxis()->GetNbins() << " "<< hist->GetXaxis()->GetNbins() << endl;
+	cout << hist->GetYaxis()->GetNbins() << " " << hist->GetXaxis()->GetNbins() << endl;
 
-	for(int i=run; i < hist->GetXaxis()->GetNbins(); ++i) 
+	for (int i = run; i < hist->GetXaxis()->GetNbins(); ++i)
 	{
-		for (int j=run; j < hist->GetYaxis()->GetNbins(); ++j)
+		for (int j = run; j < hist->GetYaxis()->GetNbins(); ++j)
 		{
-			count = hist->GetBinContent(i,j);
+			count = hist->GetBinContent(i, j);
 			if (count > 0)
 			{
-				double x,y;
-				y = ((TAxis*)hist->GetYaxis())->GetBinCenter(j);
-				x = ((TAxis*)hist->GetXaxis())->GetBinCenter(i);
+				double x, y;
+				y = ((TAxis *)hist->GetYaxis())->GetBinCenter(j);
+				x = ((TAxis *)hist->GetXaxis())->GetBinCenter(i);
 				g->SetMarkerColor(1);
-				g->SetPoint(g->GetN(),x,y);
-				cout << count << " " << i << "/" << Bin_X << " " << j << "/" << Bin_Y << " " << "\r";
+				g->SetPoint(g->GetN(), x, y);
+				cout << count << " " << i << "/" << Bin_X << " " << j << "/" << Bin_Y << " "
+					 << "\r";
 			}
 		}
 	}
 
 	g->Draw("AP");
-	Ellipse(run,g);
+	Ellipse(run, g);
 }
